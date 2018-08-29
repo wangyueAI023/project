a7165e14a8f9bde7dfc29d550e32b69f4aefdbda
wangyueAI023 <wangyue1023@outlook.com>
2018-08-29 22:41:32 +0800

    kaola

--- 网易考拉/kaola nav/JS/script.js
+++ 网易考拉/kaola nav/JS/script.js
@@ -1,5 +1,6 @@
 //var oShow = document.getElementById('show');
 window.onload = function(){
+var oShow = document.getElementById('show');
 var pic=['0pLKZeHD1mx2v31NPCL0p4T1808091712_1920_506.jpg',
          '4JFuZkWWXTu3Gu4SiZO2VCsZ-pcT1808091627_1920_506.jpg',
          '4ThdwIGA7eaaHTlOTTCVPCNiFwT1808091449_1920_506.jpg',
@@ -8,11 +9,11 @@ var pic=['0pLKZeHD1mx2v31NPCL0p4T1808091712_1920_506.jpg',
          'XhF8n2IFExKkABE68rv10hbPCwOes6U1AT1808091109_1920_506.jpg'];
 i=0;
 function begin()	{
-	show.src="images/"+pic[i];
+	oShow.src="images/"+pic[i];
 	i++;
 	if (i==6) {i=0}
 	console.log(i,pic[i]);
 }
-setInterval(begin,3000);
+setInterval(begin,1000);
 }
 
